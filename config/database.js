02f6784ccb21cd4cc44f355f63cbece5c3075191
Ramiro Solis <h1.solis.castillo.ramiro@gmail.com>
2018-04-11 15:47:50 -0600

    Se arregla problema con la base de datos

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     'url' : 'mongodb://localhost/scrumio', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'urlPro' : 'mongodb://ramiro:123456789@ds139725.mlab.com:39725/heroku_x0cg2jhg'
+    'urlPro' : 'mongodb://ramiro:123456789@ds033096.mlab.com:33096/scrumio'
 };

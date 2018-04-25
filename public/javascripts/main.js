a9d27b8b28c4cda91faee1686059286afe4a8352
Ramiro Solis <h1.solis.castillo.ramiro@gmail.com>
2018-04-24 22:14:34 -0600

    Proyectos terminado

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,21 @@
 $(document).ready(() =>{
-  console.log("In");
+  $("#closeSideModal").click(()=>{
+    $("#mySidenav").width("0px");
+    $("#mySidenav").css("overflow-y","hidden");
+    $("#overlay").width("0");
+    $("#overlay").css("opacity","0.9");
+  });
+
+  $("#openSideModal").click(()=>{
+    $("#mySidenav").width("521px");
+    $("#mySidenav").css("overflow-y","visible");
+    $("#overlay").width("100%");
+    $("#overlay").css("opacity","0.5");
+  });
+  $("#overlay").click(()=>{
+    $("#mySidenav").width("0px");
+    $("#mySidenav").css("overflow-y","hidden");
+    $("#overlay").width("0");
+    $("#overlay").css("opacity","0.9");
+  });
 });

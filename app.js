0a68308b19672893644b09d765e883fd4b549ff4
Ramiro Solis <h1.solis.castillo.ramiro@gmail.com>
2018-04-26 09:39:24 -0600

    Se agrega modal para agregar equipos

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/js', express.static(__dirname + '/node_modules/bootbox')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 // error handler
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,4 +18,28 @@ $(document).ready(() =>{
     $("#overlay").width("0");
     $("#overlay").css("opacity","0.9");
   });
+
+  $("#addTeam").click(()=>{
+    body = `<div id="modalAddTeam">
+              <h4>Crear Nuevo Equipo</h4>
+              <div class="form-group">
+                <label>Nombre</label>
+                <input id="createTeam" type="text" placeholder="Escribe el nombre de tu proyecto" class="form-control"/>
+              </div>
+              <h5>Seleccionar Miembros</h5>
+              <div class="choose-team">
+                <img src="images/abrazame.png" class="img-fluid"/>
+                <p>Oh oh! Parece que aún no tienes miembros para armar tu equipo! Agrega unos desde la pestaña de miembros o da clic <a href="#">aqui</a>.</p>
+              </div>
+              <div class="float-right">
+                <button class="closeModal">Cancelar</button>
+              </div>
+            </div>`;
+
+    var dialog = bootbox.dialog({
+      message: body,
+      closeButton: true
+    });
+  });
+
 });

8fdf7c35e9d6586ee93842ce67b60871cd98588d
Melissa Rocha <emelissaro97@gmail.com>
2018-05-11 21:18:08 -0600

    Se crea la funcion update y get para los proyectos

--- controllers/proyectsController.js
+++ controllers/proyectsController.js
@@ -2,6 +2,28 @@ const express = require('express');
 const Proyect = require('../models/proyect');
 const mongoose = require('mongoose');
 
+function index(req, res, next){
+  const page = req.params.page ? req.params.page : 1;
+  Proyect.paginate({}, {
+    page: page,
+    limit:3
+  }, (err, users)=>{
+    if(err){
+      res.json({
+        err: true,
+        message: 'No se puedieron extraer los proyectos',
+        objs: {}
+      });
+    }else{
+      res.json({
+        err: false,
+        message:'Lista de proyectos',
+        objs:users
+      });
+    }
+  });
+}
+
 function create(req, res, next){
   const nombre = req.body.name;
   const date_request = req.body.date_request;
@@ -10,16 +32,16 @@ function create(req, res, next){
   const product_owner_id = req.user._id;
   const description = req.body.description;
 
-  let proyecto = new Proyect();
+  let proyect = new Proyect();
 
-  proyecto.nombre = nombre;
-  proyecto.date_request = date_request;
-  proyecto.date_deployed = date_deployed;
-  proyecto.product_owner = product_owner;
-  proyecto.product_owner_id = product_owner_id;
-  proyecto.description = description;
+  proyect.nombre = nombre;
+  proyect.date_request = date_request;
+  proyect.date_deployed = date_deployed;
+  proyect.product_owner = product_owner;
+  proyect.product_owner_id = product_owner_id;
+  proyect.description = description;
 
-  proyecto.save((err, proyecto)=>{
+  proyect.save((err, proyect)=>{
     if (err) {
       res.json({
         err: true,
@@ -30,7 +52,42 @@ function create(req, res, next){
       res.json({
         err: false,
         message:'Proyecto Guardado',
-        objs: proyecto
+        objs: proyect
+      });
+    }
+  });
+}
+
+function update(req, res, next){
+  const nombre = req.body.name;
+  const date_request = req.body.date_request;
+  const date_deployed = req.body.date_deployed;
+  const product_owner = req.body.product_owner;
+  const product_owner_id = req.user._id;
+  const description = req.body.description;
+
+  let proyect = {};
+
+  proyect.nombre = nombre;
+  proyect.date_request = date_request;
+  proyect.date_deployed = date_deployed;
+  proyect.product_owner = product_owner;
+  proyect.product_owner_id = product_owner_id;
+  proyect.description = description;
+
+  Proyect.findOneAndUpdate({ _id : req.params.id}, {$set: proyect}, {new: true},
+    (err, proyect)=>{
+    if (err) {
+      res.json({
+        err: true,
+        message: 'No se pudo editar el proyecto',
+        objs: err
+      });
+    }else{
+      res.json({
+        err: false,
+        message:'Proyecto Editado',
+        objs: proyect
       });
     }
   });
@@ -38,7 +95,7 @@ function create(req, res, next){
 
 function show(req, res, next){
   const id = req.params.id;
-  Proyecto.findOne({
+  Proyect.findOne({
     _id:id
   }, (err, obj)=>{
     res.json({
@@ -78,7 +135,9 @@ function remove(req, res, next){
 
 
 module.exports = {
+  index,
   create,
+  update,
   show,
   remove
 }

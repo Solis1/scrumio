6639f1f5256d15247270533211835c7b4c00fbe4
Melissa Rocha <emelissaro97@gmail.com>
2018-05-11 21:16:12 -0600

    Se agregan rutas para las diferentes acciones de los proyectos

--- routes/proyects.js
+++ routes/proyects.js
@@ -3,7 +3,10 @@ const securityMiddleware = require('../middlewares/securityMiddleware');
 
 module.exports = function(app, passport) {
 
+  app.get('/proyects', securityMiddleware.isLoggedIn, proyectsControllers.index);
   app.post('/proyects', securityMiddleware.isLoggedIn, proyectsControllers.create);
+  app.put('/proyects/:id', securityMiddleware.isLoggedIn, proyectsControllers.update);
+  app.delete('/proyects/:id' securityMiddleware.isLoggedIn, proyectsControllers.remove);
 
 }
 
